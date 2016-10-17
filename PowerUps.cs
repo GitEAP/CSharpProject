ba5760713dc94d3671c5596802316e44e730e847
Erick Perez <NBA_EAP@hotmail.com>
2016-10-17 11:50:41 -0600

    added files to new project

---
+++ PowerUps.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class PowerUps {
+    int power = 10;
+    int duration = 5;
+
+    string name = "PowerUp";
+
+    public void RunPowerUp () {
+        Console.WriteLine("using PowerUp");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static PowerUps Health;
+        public static PowerUps Ammo;
+        public static void Main(string[] args)
+        {
+            Health.RunPowerUp();
+            Ammo.RunPowerUp();
+        }
+    }
+}
